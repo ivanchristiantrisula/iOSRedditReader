3a6ff4c3d30ba61fa5d07819e8d2e595e7a8828e
Ivan <ivanchristiantrisula@outlook.com>
2021-10-25 21:59:24 +0700

    Initial Commit

---
+++ RedditReader/RedditReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedditReaderApp.swift
+//  RedditReader
+//
+//  Created by Ivan Christian on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct RedditReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
